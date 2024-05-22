c6885f67a8e544c45d84e2758a1ddefe3c4b03db
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-05-22 00:12:17 -0400

    signing up works

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,41 @@
-use std::io
+use std::io::{self, Write};
 
-
-
-// fn login() -> (String, String) {
-//     println!("Enter your username: ");
-//     let mut username = String::new();
-//     std::io::stdin().read_line(&mut username).unwrap();
-//     let username = username.trim();
-
-//     println!("Enter your password: ");
-//     let mut password = String::new();
-//     std::io::stdin().read_line(&mut password).unwrap();
-//     let password = password.trim();
-
-//     (username.to_string(), password.to_string())
-// }
-
-
-
-fn sign_up(String, String) -> (String, String){
+fn sign_up() -> (String, String) {
     println!("Enter your username: ");
     let mut username = String::new();
-    std::io::stdin().read_line(&mut username).unwrap();
-    let username = username.trim();
+    io::stdin().read_line(&mut username).unwrap();
+    let username = username.trim().to_string();
 
     println!("Enter your password: ");
     let mut password = String::new();
-    std::io::stdin().read_line(&mut password).unwrap();
-    let password = password.trim();
+    io::stdin().read_line(&mut password).unwrap();
+    let password = password.trim().to_string();
 
-    println!("Username: {}, Password: {}", username, password);
-
+    (username, password)
 }
 
-
 fn main() {
-
     println!("Welcome to Knowledge");
     println!("---------------------");
 
-
     println!("Do you have a login? (y/n)");
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
     let input = input.trim();
 
     match input {
         "y" => {
-            println!("Please create a login");
+            println!("Please enter your login details");
             // create_login();
         },
         "n" => {
             println!("Sign up");
-            sign_up();
+            let (username, password) = sign_up();
+            println!("Welcome {}!", username, );
         },
+        _ => {
+            println!("Invalid input. Please enter 'y' or 'n'.");
+        }
     }
-
 }
+
---
+++ target/debug/rust_ssh_app.d
@@ -0,0 +1,1 @@
+/Users/seifmamdouh/rust-ssh/rust_ssh_app/target/debug/rust_ssh_app: /Users/seifmamdouh/rust-ssh/rust_ssh_app/src/main.rs
